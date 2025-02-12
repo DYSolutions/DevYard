d0c7aeefb398cd6333866a9540af1c3c438b9230
coops15 <YourGitHubEmail>
2025-02-12 22:27:55 +0530

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import Herobg from "./components/heroBg"
+import About from "./components/about"
 
 export default function Home() {
   return (
     <div>
 
-      <Herobg/>
-
+      <Herobg />
+      <About />
     </div>
   );
 }
