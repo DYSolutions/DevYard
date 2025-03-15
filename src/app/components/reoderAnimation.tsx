bac82b1096bd8e4b1d75c32a6e5f79dc588a2909
coops15 <worldhidden0@gmail.com>
2025-03-15 11:13:02 +0530

    update reorder animation

--- src/app/components/reoderAnimation.tsx
+++ src/app/components/reoderAnimation.tsx
@@ -1,59 +1,57 @@
-"use client"
+"use client";
 
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 import { useEffect, useState } from "react";
-import Image from "next/image";
-import tailwind from "../../../public/images/tailwind.png"
-import fb from "../../../public/images/fb.png"
-import react from "../../../public/images/react.png"
-import javaScript from "../../../public/images/javaScript.png"
-import flutter from "../../../public/images/flutter.png"
-import git from "../../../public/images/git.png"
-import python from "../../../public/images/python.png"
-import android from "../../../public/images/android.png"
-import ts from "../../../public/images/ts.png"
+import Image, { StaticImageData } from "next/image";
+import tailwind from "../../../public/images/tailwind.png";
+import fb from "../../../public/images/fb.png";
+import react from "../../../public/images/react.png";
+import javaScript from "../../../public/images/javaScript.png";
+import flutter from "../../../public/images/flutter.png";
+import git from "../../../public/images/git.png";
+import python from "../../../public/images/python.png";
+import android from "../../../public/images/android.png";
+import ts from "../../../public/images/ts.png";
 
 export default function Reordering() {
     const [order, setOrder] = useState(initialOrder);
 
     useEffect(() => {
-        const timeout = setTimeout(() => setOrder(shuffle(order)), 1000);
-        return () => clearTimeout(timeout);
-    }, [order]);
+        const interval = setInterval(() => {
+            setOrder((prevOrder) => shuffle(prevOrder));
+        }, 2000);
+
+        return () => clearInterval(interval);
+    }, []);
 
     return (
         <ul style={container}>
-            {order.map((src,index) => (
-                <motion.li
-                    key={index}
-                    layout
-                    transition={spring}
-                    style={item}
-                >
-                    <Image src={src} alt="Reordering Item" style={imageStyle} />
-                </motion.li>
-            ))}
+            <AnimatePresence>
+                {order.map((src, index) => (
+                    <motion.li
+                        key={src.src} // Ensure key is unique
+                        layout
+                        initial={{ opacity: 0, scale: 0.8, y: 10 }}
+                        animate={{ opacity: 1, scale: 1, y: 0 }}
+                        exit={{ opacity: 0, scale: 0.8, y: -10 }}
+                        transition={spring}
+                        style={item}
+                    >
+                        <Image src={src} alt="Reordering Item" style={imageStyle} />
+                    </motion.li>
+                ))}
+            </AnimatePresence>
         </ul>
     );
 }
 
-const initialOrder = [
-    tailwind,
-    fb,
-    react,
-    javaScript,
-    flutter,
-    git,
-    python,
-    android,
-    ts
-];
+const initialOrder = [tailwind, fb, react, javaScript, flutter, git, python, android, ts];
 
 /**
  * ==============   Utils   ================
  */
-function shuffle([...array]) {
-    return array.sort(() => Math.random() - 0.5);
+function shuffle(array: StaticImageData[]) {
+    return [...array].sort(() => Math.random() - 0.5);
 }
 
 /**
@@ -61,8 +59,9 @@ function shuffle([...array]) {
  */
 const spring = {
     type: "spring",
-    damping: 20,
-    stiffness: 300,
+    damping: 15,
+    stiffness: 120, // Lower stiffness for smoother effect
+    mass: 0.5, // Adding mass for more natural animation
 };
 
 const container: React.CSSProperties = {
@@ -76,17 +75,19 @@ const container: React.CSSProperties = {
     width: 500,
     flexDirection: "row",
     justifyContent: "center",
-    alignItems: "center"
+    alignItems: "center",
 };
 
-const item = {
+const item: React.CSSProperties = {
     width: 150,
     height: 150,
     borderRadius: "10px",
     overflow: "hidden",
     display: "flex",
     justifyContent: "center",
-    alignItems: "center"
+    alignItems: "center",
+    // backgroundColor: "white",
+    // boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.1)",
 };
 
 const imageStyle: React.CSSProperties = {
